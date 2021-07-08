47cc6fdc1e8d8fa0976aa1e7994d43281c2df746
oleksiikrutko <krutko.av@gmail.com>
2021-07-09 00:23:23 +0300

    version 3.1  change CalculateBasketTest

--- src/test/java/ua/goit/service/CalculateBasketTest.java
+++ src/test/java/ua/goit/service/CalculateBasketTest.java
@@ -7,20 +7,11 @@ import org.mockito.Mockito;
 import org.mockito.stubbing.OngoingStubbing;
 
 public class CalculateBasketTest {
-    private CalculateBasket calculateBasket;
+    private CalculateBasketIntf calculateBasketIntf;
 
     @Before
     public void createGoods(){
-        calculateBasket = new CalculateBasket();
-    }
-
-    @Test
-    public void testSumOfBasket() {
-
-        String basket = "ABBCCCDDDDF";
-
-        CalculateBasketIntf calculateBasketIntf = Mockito.mock(CalculateBasketIntf.class);
-
+        calculateBasketIntf = Mockito.mock(CalculateBasketIntf.class);
         Mockito.when(calculateBasketIntf.sumOfBasket("A")).thenReturn(1.25);
         Mockito.when(calculateBasketIntf.sumOfBasket("AAA")).thenReturn(3.0);
         Mockito.when(calculateBasketIntf.sumOfBasket("B")).thenReturn(4.25);
@@ -28,7 +19,17 @@ public class CalculateBasketTest {
         Mockito.when(calculateBasketIntf.sumOfBasket("CCCCC")).thenReturn(5.0);
         Mockito.when(calculateBasketIntf.sumOfBasket("D")).thenReturn(0.75);
         Mockito.when(calculateBasketIntf.sumOfBasket("C")).thenReturn(1.0);
-        Mockito.when(calculateBasketIntf.sumOfBasket(basket)).thenReturn(15.75);
+        Mockito.when(calculateBasketIntf.sumOfBasket("ABBCCCDDDDF")).thenReturn(15.75);
+    }
+
+    @Test
+    public void testSumOfBasket() {
+
+        Assert.assertEquals(1.25, calculateBasketIntf.sumOfBasket("A"), 0.0000000001);
+        Assert.assertEquals(4.25, calculateBasketIntf.sumOfBasket("B"), 0.0000000001);
+        Assert.assertEquals(1.0, calculateBasketIntf.sumOfBasket("C"), 0.0000000001);
+        Assert.assertEquals(0.75, calculateBasketIntf.sumOfBasket("D"), 0.0000000001);
+        Assert.assertEquals(3.0, calculateBasketIntf.sumOfBasket("AAA"), 0.0000000001);
 
     }
 
